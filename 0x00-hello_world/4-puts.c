d6b6403f6c2f34fad1c18566b4236fe7d2b975d6
Cynthia Kemunto <kemuntokerosi.com>
2023-08-13 03:31:55 -0700

    Update the output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Prints a string
+ * return (0)
+ */
 int main(void)
 {
-	char *msg = "Programming is like building a multilingual puzzle";
+	char *msg = "\"Programming is like building a multilingual puzzle";
+
 	puts(msg);
 	return (0);
 }
