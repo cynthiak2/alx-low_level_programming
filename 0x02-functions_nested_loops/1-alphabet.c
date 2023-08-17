019180e66a518d773cb51e7d03f07d46d6751970
Cynthia Kemunto <kemuntokerosi.com>
2023-08-17 07:44:49 -0700

    Use the betty coding style

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * print_alphabet - prints alphabets from a to z
- * 
+ *
  * Return: nothing
  */
 void print_alphabet(void)
 {
 	char c;
 
-	for(c = 'a'; c <= 'z';c++)
+	for (c = 'a'; c <= 'z'; c++)
 		_putchar(c);
 	_putchar('\n');
 }
